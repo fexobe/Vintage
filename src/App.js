351852d909bb0312e26edfeb327ca7a52aef733d
Alisha <fexobe@gmail.com>
2020-12-10 12:00:55 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import './main.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     
+    
+<body>
+  
+<div id="logo">Not Your Mom's Vintage Clothing | A Vintage Clothing Blog</div>
+
+<header className="header">
+
+<ul class="menu">
+        
+            <li>
+                <a href="#">Home</a>
+                <a href="#">Begining</a>
+                <a href="#">Influences</a>
+                <a href="#">Recommendations</a>
+            </li>
+        </ul>
+  </header>   
+
+
+
+
+{/*******Welcome*****/}    
+     <div className="welcome">
+        <p>If you are a fan of clothing from the 1940's and 1950's you are in the right place. Here we talk about all things vintage from that time period. Follow weekly for updates</p>
+     </div>
+
+{/*****Imgage goes here*****/}
+
+{/*****Introduction*****/}
+    <div className="Introduction">
+      <h2>How it all began</h2>
+      <p>My road to loving vintage is probably not that abnormal in most respects. I never really cared about the costumes or clothing from movies or the past. This year has been an eye opener for me.  At the beginning of the year we had a very unwelcome visitor on our planet. No, not an alien invasion but one just as bad.  We entered a Pandemic.  Covid 19 has changed almost everything in our lives these days. We had a lockdown that lasted for four months.  That lockdown ended only to enter another lockdown this last month. With all these lockdowns most of us if not all of us have had to find new ways to entertain ourselves. Like many people,Youtube was the answer for my bordom.  I stared surfing through different channels and landed on a channel by a Lady named Abby Cox.  If you don't know who she is,  I'll tell you.  She is a historical dressmaker and an historical educator who dressed in 18th century clothing full time for five years.  I was fasinated by her humor, sewing skills and her cloths. I have some basic sewing skills so in the beginning sewing was the draw. The more videos I watched the more interested I became in the clothing.  One day she recommended a lady named Rachel Maksey's channel for her viewers. I of course surfed over to her channel and that is all she wrote.</p>
+
+    </div>
+
+{/*****Opinion*****/}
+<div className="My-Opinion">
+<h2>My Opinion</h2>
+
+
+</div>
+
+{/*****Influences*****/}
+
+<div className="My-Influences">
+
+  <h2>Influences</h2>
+
+</div>
+
+{/*****Recomendations*****/}
+
+<div className="My-Recommendations">
+
+  <h2>Recommendations</h2>
+</div>
+
+</body>
+{/*****Footer*****/}
+        <footer>Created by Alisha Jacobs &copy; 2020
+           <p><a href="mailto:fexobe@gmail.com">Contact Alisha</a></p>
+
+        </footer>
+      
       </header>
+      
     </div>
   );
 }
